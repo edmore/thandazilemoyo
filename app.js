ba76061db10c24771d6e95f930ab5b23c177f0f9
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-08-02 21:49:46 +0200

    Adding dependencies and Procfile

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-var express = require('express');
+// Module Dependencies
+var express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib');
+
 var app = express();
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(stylus.middleware(
+  { src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
 
 // Define Routes
 app.get('/', function(req, res){
 	res.send('Thandazile Moyo');
     });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
